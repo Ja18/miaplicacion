0464c80c49350de83f517df80b956fae422f7c3f
Jose Adrian Alvarado <josealvarado@unicomfacauca.edu.co>
2018-03-06 17:17:52 -0500

    mensaje

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,9 @@
 /* SystemJS module definition */
+/*declare las dos primeras líneas*/
 declare var module: NodeModule;
+declare var jQuery: any;
+declare var $:any;
+
 interface NodeModule {
   id: string;
 }
