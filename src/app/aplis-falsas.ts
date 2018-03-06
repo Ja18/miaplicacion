a092e9e29661a49a4aeb9bd62c34d19688262a5a
Jose Adrian Alvarado <josealvarado@unicomfacauca.edu.co>
2018-03-05 22:17:53 -0500

    Mensaje

--- src/app/aplis-falsas.ts
+++ src/app/aplis-falsas.ts
@@ -1,11 +1,11 @@
 import { Apli } from './apli';
 
 export const APLIS: Apli[] = [
-    {id: 11, nombre:'Aplicacion 1'},
-    {id: 12, nombre:'Aplicacion 2'},
-    {id: 13, nombre:'Aplicacion 3'},
-    {id: 14, nombre:'Aplicacion 4'},
-    {id: 15, nombre:'Aplicacion 5'}
+    {id: 11, nombre:'Aplicación 1'},
+    {id: 12, nombre:'Aplicación 2'},
+    {id: 13, nombre:'Aplicación 3'},
+    {id: 14, nombre:'Aplicación 4'},
+    {id: 15, nombre:'Aplicación 5'}
   ];
   
   
